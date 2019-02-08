5674d5db099cdb7742085ef2e524464d38d09126
Emanuel Wanyonyi <wanyonyi.d.emanuel@gmail.com>
2019-02-08 12:22:18 +0300

    Create run_tests.py

---
+++ run_tests.py
@@ -0,0 +1,9 @@
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('develop/tests/v1', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+if __name__ == '__main__':
+    run()
