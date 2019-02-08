96a67d32b8b7e4f539d803e1ed1500c883edff8d
emanuelwanyonyi <wanyonyi.d.emanuel@gmail.com>
2019-02-08 09:12:10 +0300

    added admin and auth apis

---
+++ tests/v1/base.py
@@ -0,0 +1,14 @@
+""" This the base testing """
+# develop/tests/v1/base.py
+
+from flask_testing import TestCase
+from develop import app
+
+
+class BaseTestCase(TestCase):
+    """ Base Tests """
+
+    def create_app(self):
+        """ Test for creating an app """
+        app.config.from_object('develop.config.TestingConfig')
+        return app
