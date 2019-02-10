37549abe586f2750e1180dadbfe2891dc1a32f29
emanuelwanyonyi <wanyonyi.d.emanuel@gmail.com>
2019-02-10 19:12:58 +0300

    commit tests changes

--- tests/v1/test_parties.py
+++ tests/v1/test_parties.py
@@ -10,48 +10,47 @@ class TestParties(unittest.TestCase):
         self.app = app
         self.client = self.app.test_client()
         self.party_data = {
-            "id" : 1,
+            "party_id" : 1,
             "name" :"party 1",
-            "hqAddress" : "Roysambu",
-            "logoUrl" : "https://file/1"
+            "hq_address" : "Roysambu",
+            "logo_url" : "https://file/1"
         }
 
     def test_createparty(self):
         """ Test for create party method """
         response = self.client.post(
-            '/api/v1/admin/createparty', data=json.dumps(self.party_data), 
+            '/api/v1/admin/parties', data=json.dumps(self.party_data), 
             content_type='application/json')
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
     def test_getallparties(self):
         """ Test for getting all parties method """
         response = self.client.get(
-            '/api/v1/admin/parties/getall', data=json.dumps(self.party_data), 
-            content_type='application/json')
+            '/api/v1/admin/parties', content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
     def getparty(self):
         """ Test for getting a specific party method """
         response = self.client.get(
-            '/api/v1/admin/parties/get/<int:party_id>', data=json.dumps(self.party_data), 
+            '/api/v1/admin/parties/<int:party_id>', data=json.dumps(self.party_data), 
             content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
     def editparty(self):
         """ Test for getting a specific party method """
-        response = self.client.post(
-            '/api/v1/admin/parties/get/<int:party_id>', data=json.dumps(self.party_data), 
+        response = self.client.put(
+            '/api/v1/admin/parties/<int:party_id>', data=json.dumps(self.party_data), 
             content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
     def deleteparty(self):
         """ Test for getting a specific party method """
-        response = self.client.post(
-            '/api/v1/admin/parties/delete/<int:party_id>', data=json.dumps(self.party_data), 
+        response = self.client.delete(
+            '/api/v1/admin/parties/<int:party_id>', data=json.dumps(self.party_data), 
             content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
