0e8257d398fb95da9af48beeabe7e04c60df6982
Emanuel Wanyonyi <wanyonyi.d.emanuel@gmail.com>
2019-02-14 09:54:38 +0300

    Update admin_models.py

--- server/api/v1/admin/admin_models.py
+++ server/api/v1/admin/admin_models.py
@@ -1,36 +1,7 @@
 """ A class for parties data Strucure """
-parties_data = [ 
-        {
-            'name':'Party 1',
-            'hq_address':'Pangani, 2nd Street, Party 1 House',
-            'logo_url':'/partielsdata/logs/party1'
-        },
-        {
-            'name':'Party 2',
-            'hq_address':'West End Building, Second Floor, Waiyaki Way',
-            'logo_url':'/partielsdata/logs/party3'
-        },
-        {
-            'name':'Party 3',
-            'hq_address':'HWZ, 3rd Floor, HouseMombasa Road, Nairobi West',
-            'logo_url':'/partielsdata/logs/party3'
-        }
-    ]
+parties_data = []
 
-offices_data = [ 
-        {
-            'office_type':'federal',
-            'name':'Office 1'
-        },
-        {
-            'office_type':'state',
-            'name':'Office 2'
-        },
-        {
-            'office_type':'legislative',
-            'name':'Office 2'
-        }
-    ]
+offices_data = []
 
 
 class Parties():
